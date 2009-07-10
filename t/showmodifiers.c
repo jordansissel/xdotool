96e1b6dccbe03ba985e3f1e90757aedea84ca170
Jordan Sissel <jls@semicomplete.com>
2009-07-10 07:57:01 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2202 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ t/showmodifiers.c
@@ -0,0 +1,25 @@
+/* Build with:
+ * gcc -I.. -lX11 -lXtst showmodifiers.c
+ */
+
+/* Yes, I know including .c files is insanity. */
+#include "xdo.c"
+
+int main(int argc, char **argv) {
+  xdo_t *xdo = NULL;
+  charcodemap_t *keys = NULL;
+  int nkeys = 0;
+  xdo = xdo_new(NULL);
+
+  while (1) {
+    int i = 0;
+    xdo_active_modifiers_to_keycode_list(xdo, &keys, &nkeys);
+    if (nkeys > 0) {
+      for (i = 0; i < nkeys; i++) {
+        printf("%d, ", keys[i].code);
+      }
+      printf("\n");
+    }
+    free(keys);
+  }
+}
