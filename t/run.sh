7ed81245adc7874fa478d4a78542b6d3597c871a
Jordan Sissel <jls@semicomplete.com>
2010-01-01 01:23:29 +0000

    - Run tests headless using Xvfb (suggested by Russell Harmon)   * Requires: Xvfb and GNOME
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2568 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- t/run.sh
+++ t/run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+if ! which Xvfb > /dev/null 2>&1 ; then
+  echo "Xvfb not found, but it is needed for the tests."
+  exit 1
+fi
+
+export DISPLAY=:15
+Xvfb $DISPLAY &
+xvfb_pid="$!"
+
+gnome-session > /dev/null 2>&1 &
+gnome_pid="$!"
+sleep 5
+
 sh no_crashes_please.sh
 sh test_search_maxdepth.sh
 sh test_set_window.sh
+kill $gnome_pid $xvfb_pid
