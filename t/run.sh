611c564199a58a74871335538acea0f26fd0b967
Jordan Sissel <jls@semicomplete.com>
2010-01-01 01:30:14 +0000

    - better checking of test success
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2569 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- t/run.sh
+++ t/run.sh
@@ -11,9 +11,26 @@ xvfb_pid="$!"
 
 gnome-session > /dev/null 2>&1 &
 gnome_pid="$!"
+
+# Give gnome a few seconds to get going...
 sleep 5
 
-sh no_crashes_please.sh
-sh test_search_maxdepth.sh
-sh test_set_window.sh
+results=$(mktemp)
+( sh no_crashes_please.sh
+  sh test_search_maxdepth.sh
+  sh test_set_window.sh
+) | tee $results
+
+if grep -q "^FAILURE:" $results ; then
+  exitcode=1
+else
+  exitcode=0
+fi
+
+echo "$(grep -c "^FAILURE:" $results) tests failed"
+echo "$(grep -c "^SUCCESS:" $results) tests passed"
+
+rm $results
 kill $gnome_pid $xvfb_pid
+
+exit $exitcode
