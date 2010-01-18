9220d9ddcd60fb1533730f2802f2b60d10ec59cf
Jordan Sissel <jls@semicomplete.com>
2010-01-18 09:00:31 +0000

    - generate versioned header
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2594 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- version.sh
+++ version.sh
@@ -3,10 +3,19 @@
   
 MAJOR="0"
 DATE="$(date +%Y%m%d)"
-REVISION=$([ -d .svn ] && svn info . | awk '/Revision:/ {print $2}')
-: ${REVISION=:0}
+#REVISION=$([ -d .svn ] && svn info . | awk '/Revision:/ {print $2}')
+REVISION=00
+
+
+VERSION="$MAJOR.$DATE.$REVISION"
 
 case $1 in
   --major) echo "$MAJOR" ;;
-  *) echo "$MAJOR.$DATE.$REVISION" ;;
+  --header) 
+    echo "#ifndef _VERSION_H_"
+    echo "#define _VERSION_H_"
+    echo "static const char *XDO_VERSION = \"$VERSION\";"
+    echo "#endif /* ifndef _VERSION_H */"
+    ;;
+  *) echo "$VERSION" ;;
 esac
