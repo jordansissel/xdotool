2f4e9456bbbb9c28883c1ba349a2af439e9dfcff
Jordan Sissel <jls@semicomplete.com>
2010-01-18 06:56:01 +0000

    - Add version
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2590 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ xdo_version.h
@@ -0,0 +1,8 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#ifndef BUILDVERSION
+#error No BUILDVERSION defined.
+#endif
+static const char *XDO_VERSION = BUILDVERSION;
+#endif /* ifndef _VERSION_H */
