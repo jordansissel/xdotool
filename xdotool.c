32caae13480bfba3694e395d839b99824e96e84b
Jordan Sissel <jls@semicomplete.com>
2010-02-28 03:07:29 +0000

    - Properly support --clearmodifiers where we said we did but didn't actually   implement.
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2678 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- xdotool.c
+++ xdotool.c
@@ -253,21 +253,31 @@ int cmd_mousemove(int argc, char **args) {
   int ret = 0;
   int x, y;
   char *cmd = *args;
+  xdo_active_mods_t *active_mods;
+  int clear_modifiers = 0;
 
   int c;
   int screen = 0;
   Window window = 0;
   static struct option longopts[] = {
+    { "clearmodifiers", no_argument, NULL, 'c' },
     { "help", no_argument, NULL, 'h' },
     { "screen", required_argument, NULL, 's' },
     { "window", required_argument, NULL, 'w' },
     { 0, 0, 0, 0 },
   };
-  static const char *usage = "Usage: %s [--window W] [--screen N] <xcoord> <ycoord>\n";
+  static const char *usage = 
+             "Usage: %s [options] <xcoord> <ycoord>\n"
+            "--clearmodifiers       - reset active modifiers (alt, etc) while typing\n"
+            "--screen SCREEN        - which screen to move on, default is current screen\n"
+            "--window <windowid>    - specify a window to send keys to\n";
   int option_index;
 
-  while ((c = getopt_long_only(argc, args, "hs:w:", longopts, &option_index)) != -1) {
+  while ((c = getopt_long_only(argc, args, "chs:w:", longopts, &option_index)) != -1) {
     switch (c) {
+      case 'c':
+        clear_modifiers = 1;
+        break;
       case 'h':
         printf(usage, cmd);
         return EXIT_SUCCESS;
@@ -296,6 +306,11 @@ int cmd_mousemove(int argc, char **args) {
   x = atoi(args[0]);
   y = atoi(args[1]);
 
+  if (clear_modifiers) {
+    active_mods = xdo_get_active_modifiers(xdo);
+    xdo_clear_active_modifiers(xdo, window, active_mods);
+  }
+
   if (window > 0) {
     ret = xdo_mousemove_relative_to_window(xdo, window, x, y);
   } else {
@@ -304,6 +319,11 @@ int cmd_mousemove(int argc, char **args) {
 
   if (ret)
     fprintf(stderr, "xdo_mousemove reported an error\n");
+
+  if (clear_modifiers) {
+    xdo_set_active_modifiers(xdo, window, active_mods);
+    xdo_free_active_modifiers(active_mods);
+  }
 
   return ret;
 }
@@ -374,14 +394,14 @@ int cmd_mousedown(int argc, char **args) {
             "--clearmodifiers       - reset active modifiers (alt, etc) while typing\n";
   int option_index;
 
-  while ((c = getopt_long_only(argc, args, "hw:", longopts, &option_index)) != -1) {
+  while ((c = getopt_long_only(argc, args, "chw:", longopts, &option_index)) != -1) {
     switch (c) {
+      case 'c':
+        clear_modifiers = 1;
+        break;
       case 'h':
         printf(usage, cmd);
         return EXIT_SUCCESS;
-        break;
-      case 'c':
-        clear_modifiers = 1;
         break;
       case 'w':
         window = strtoul(optarg, NULL, 0);
@@ -441,7 +461,7 @@ int cmd_mouseup(int argc, char **args) {
             "--clearmodifiers       - reset active modifiers (alt, etc) while typing\n";
   int option_index;
 
-  while ((c = getopt_long_only(argc, args, "h", longopts, &option_index)) != -1) {
+  while ((c = getopt_long_only(argc, args, "cw:h", longopts, &option_index)) != -1) {
     switch (c) {
       case 'h':
         printf(usage, cmd);
@@ -547,7 +567,7 @@ int cmd_click(int argc, char **args) {
             "--clearmodifiers       - reset active modifiers (alt, etc) while typing\n";
   int option_index;
 
-  while ((c = getopt_long_only(argc, args, "h", longopts, &option_index)) != -1) {
+  while ((c = getopt_long_only(argc, args, "cw:h", longopts, &option_index)) != -1) {
     switch (c) {
       case 'h':
         printf(usage, cmd);
