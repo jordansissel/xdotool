2feb17ad4400eff64c6a072901eb4fa24e197cb9
Jordan Sissel <jls@semicomplete.com>
2010-05-25 17:52:54 +0000

    - Skip tests when we don't have the tools to run them.   Reported by Daniel Kahn Gillmor on the mailing list.
    git-svn-id: https://semicomplete.googlecode.com/svn/xdotool@2889 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ t/check-for-tools.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+XSERVERNAME=${XSERVER%% *}
+WMNAME=${WM%% *}
+
+if ! which $XSERVERNAME > /dev/null 2>&1 ; then
+  echo "Can't find $XSERVERNAME, skipping..." >&2
+  exit 1
+fi
+
+if [ ! -z "$WMNAME" ] && ! which $WMNAME > /dev/null 2>&1 ; then
+  echo "Can't find $WMNAME, skipping..." >&2
+  exit 1
+fi
