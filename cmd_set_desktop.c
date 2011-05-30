1ebacea4d32e2e0e2c1cb945b920e2deda52394f
Jordan Sissel <jls@semicomplete.com>
2011-05-29 21:10:01 -0700

    - add --relative flag to set_desktop.

--- cmd_set_desktop.c
+++ cmd_set_desktop.c
@@ -3,22 +3,34 @@
 int cmd_set_desktop(context_t *context) {
   char *cmd = *context->argv;
   long desktop;
+  int relative = False;
 
   int c;
+  typedef enum {
+    opt_unused, opt_help, opt_relative
+  } optlist_t;
+
   static struct option longopts[] = {
-    { "help", no_argument, NULL, 'h' },
+    { "help", no_argument, NULL, opt_help },
+    { "relative", no_argument, NULL, opt_relative },
     { 0, 0, 0, 0 },
   };
-  static const char *usage = "Usage: %s desktop\n";
+  static const char *usage = 
+    "Usage: %s desktop\n"
+    "--relative    - Move relative to the current desktop. Negative values OK\n";
   int option_index;
 
   while ((c = getopt_long_only(context->argc, context->argv, "+h",
                                longopts, &option_index)) != -1) {
     switch (c) {
       case 'h':
+      case opt_help:
         printf(usage, cmd);
         consume_args(context, context->argc);
         return EXIT_SUCCESS;
+        break;
+      case opt_relative:
+        relative = True;
         break;
       default:
         fprintf(stderr, usage, cmd);
@@ -36,6 +48,19 @@ int cmd_set_desktop(context_t *context) {
   desktop = strtol(context->argv[0], NULL, 0);
 
   consume_args(context, 1);
+
+  if (relative == True) {
+    long cur_desktop = 0, ndesktops = 0;
+    xdo_get_current_desktop(context->xdo, &cur_desktop);
+    xdo_get_number_of_desktops(context->xdo, &ndesktops);
+
+    desktop = (desktop + cur_desktop) % ndesktops;
+
+    /* negative mod doesn't result in a positive number. Fix that. */
+    if (desktop < 0) 
+      desktop += ndesktops;
+  }
+
   return xdo_set_current_desktop(context->xdo, desktop);
 }
 
