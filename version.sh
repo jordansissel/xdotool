888d15ba4972a29bfd89b364f080701a7f7a1ccb
CSylvain <chrissylvain+gh@gmail.com>
2018-01-05 21:31:26 -0500

    Add files via upload
    3.20180106.1   - added 'return' -> 'Return', 'enter' -> 'Return'  mappings in xdo_util     to eliminate user gotchas not in man page.     removed extraneous '+' chars in version.sh; already present in DATE_FMT

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ if [ -z "$MAJOR" -o -z "$RELEASE" -o -z "$REVISION" ] ; then
   MAJOR="3"
   SOURCE_DATE_EPOCH="${SOURCE_DATE_EPOCH:-$(date +%s)}"
   DATE_FMT="+%Y%m%d"
-  RELEASE="$(date -u -d "@$SOURCE_DATE_EPOCH" "+$DATE_FMT" 2>/dev/null || date -u -r "$SOURCE_DATE_EPOCH" "+$DATE_FMT" 2>/dev/null || date -u "+$DATE_FMT")"
+  RELEASE="$(date -u -d "@$SOURCE_DATE_EPOCH" "$DATE_FMT" 2>/dev/null || date -u -r "$SOURCE_DATE_EPOCH" "$DATE_FMT" 2>/dev/null || date -u "$DATE_FMT")"
   REVISION=1
   #$([ -d .svn ] && svn info . | awk '/Revision:/ {print $2}')
   : ${REVISION=:0}
