ffe0d736bc471dc0acf67efef14653a239f068c6
Antonio Russo <aerusso@aerusso.net>
2021-02-13 00:31:46 -0700

    Add windowquit option and get tests working (#306)
    * Add windowquit option
    
    Send a _NET_WINDOW_CLOSE event to a given window.  This allows for a
    graceful window shutdown, as opposed to the existing windowclose, which
    is too rude, and windowkill, which is too lethal.
    
    Signed-off-by: Antonio Russo <aerusso@aerusso.net>
    
    * Add assert_not_equal ruby test function
    
    assert_not_equal is not defined in minitest. Provide it in
    xdo_test_helper.rb.
    
    Signed-off-by: Antonio Russo <aerusso@aerusso.net>

--- xdotool.c
+++ xdotool.c
@@ -248,6 +248,7 @@ struct dispatch {
   { "windowfocus", cmd_windowfocus, },
   { "windowkill", cmd_windowkill, },
   { "windowclose", cmd_windowclose, },
+  { "windowquit", cmd_windowquit, },
   { "windowmap", cmd_windowmap, },
   { "windowminimize", cmd_windowminimize, },
   { "windowmove", cmd_windowmove, },
