690ba8128d59f440df48f358751d83aa16afb66a
stayup <holla@disnig.ga>
2022-08-28 08:41:41 -0500

    Update to this decade

--- examples/desktopconsole.sh
+++ examples/desktopconsole.sh
@@ -3,9 +3,9 @@
 
 WINDOWID=$(xdotool selectwindow)
 
-xdotool set_window --overrideredirect 1 $WINDOWID windowunmap $WINDOWID windowmap $WINDOWID
-xdotool windowsize $WINDOWID 10 100%
+xdotool set_window --overrideredirect 1 "$WINDOWID" windowunmap "$WINDOWID" windowmap "$WINDOWID"
+xdotool windowsize "$WINDOWID" 10 100%
 
 # Set behaviors
-xdotool behave $WINDOWID mouse-enter windowfocus windowsize --usehints 80 100% &
-xdotool behave $WINDOWID mouse-leave windowsize 4 100% &
+xdotool behave "$WINDOWID" mouse-enter windowfocus windowsize --usehints 80 100% &
+xdotool behave "$WINDOWID" mouse-leave windowsize 4 100% &
